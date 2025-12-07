1e933d9afc47fec32ef3e14834bb94355fe302b2
Amine Mike <amine.el-maalouf@epita.fr>
2025-12-07 23:05:03 +0100

    feat(k3s): add run

--- src/kubernetes/k3s_builder.py
+++ src/kubernetes/k3s_builder.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+import os
+import shutil
+import subprocess
 import yaml
 
 
@@ -54,7 +57,7 @@ def create_k3s_project(
 ) -> K3sProject:
     namespace = K3sNamespace(name=name)
     kustomization = K3sKustomization(
-        namespace=namespace, resources=list(modules.keys())
+        namespace=namespace, resources=["namespace.yml"] + list(modules.keys())
     )
     module_list = [
         K3sModule(
@@ -66,7 +69,9 @@ def create_k3s_project(
                 docker_image=docker_image,
                 port=start_port + i,
             ),
-            kustomization=["deployment.yml", "service.yml"],
+            kustomization=K3sKustomization(
+                namespace=namespace, resources=["deployment.yml", "service.yml"]
+            ),
             service=K3sService(
                 namespace=namespace, name=module_name, port=start_port + i
             ),
@@ -79,7 +84,7 @@ def create_k3s_project(
     )
 
 
-def k3s_deployment_to_yaml(deployment: K3sDeployment) -> str:
+def k3s_deployment_to_yaml(deployment: K3sDeployment, **kwargs) -> str:
     labels = {
         "app": deployment.namespace.name,
         "component": deployment.name,
@@ -116,21 +121,23 @@ def k3s_deployment_to_yaml(deployment: K3sDeployment) -> str:
                     },
                 },
             },
-        }
+        },
+        **kwargs,
     )
 
 
-def k3s_kustomization_to_yaml(kustomization: K3sKustomization) -> str:
+def k3s_kustomization_to_yaml(kustomization: K3sKustomization, **kwargs) -> str:
     return yaml.dump(
         {
             "kind": "Kustomization",
             "namespace": kustomization.namespace.name,
             "resources": kustomization.resources,
-        }
+        },
+        **kwargs,
     )
 
 
-def k3s_namespace_to_yaml(namespace: K3sNamespace) -> str:
+def k3s_namespace_to_yaml(namespace: K3sNamespace, **kwargs) -> str:
     return yaml.dump(
         {
             "apiVersion": "v1",
@@ -138,11 +145,12 @@ def k3s_namespace_to_yaml(namespace: K3sNamespace) -> str:
             "metadata": {
                 "name": namespace.name,
             },
-        }
+        },
+        **kwargs,
     )
 
 
-def k3s_service_to_yaml(service: K3sService) -> str:
+def k3s_service_to_yaml(service: K3sService, **kwargs) -> str:
     return yaml.dump(
         {
             "apiVersion": "v1",
@@ -163,5 +171,54 @@ def k3s_service_to_yaml(service: K3sService) -> str:
                     }
                 ],
             },
-        }
+        },
+        **kwargs,
+    )
+
+
+def dump_k3s_project(project: K3sProject, path: str = "/tmp/k3s_projects/"):
+    path += "/" + project.namespace.name
+
+    shutil.rmtree(path, ignore_errors=True)
+    os.makedirs(path)
+
+    with open(f"{path}/namespace.yml", "w") as file:
+        k3s_namespace_to_yaml(project.namespace, stream=file, default_flow_style=False)
+
+    with open(f"{path}/kustomization.yml", "w") as file:
+        k3s_kustomization_to_yaml(
+            project.kustomization, stream=file, default_flow_style=False
+        )
+
+    for module in project.modules:
+        module_path = f"{path}/{module.name}"
+        os.mkdir(module_path)
+
+        with open(f"{module_path}/deployment.yml", "w") as file:
+            k3s_deployment_to_yaml(
+                module.deployment, stream=file, default_flow_style=False
+            )
+
+        with open(f"{module_path}/kustomization.yml", "w") as file:
+            k3s_kustomization_to_yaml(
+                module.kustomization, stream=file, default_flow_style=False
+            )
+
+        with open(f"{module_path}/service.yml", "w") as file:
+            k3s_service_to_yaml(module.service, stream=file, default_flow_style=False)
+
+
+def run_k3s_project(project: K3sProject):
+    path = "/tmp/k3s_projects"
+    dump_k3s_project(project=project, path=path)
+    subprocess.run(
+        [
+            "kubectl",
+            "apply",
+            "-n",
+            project.namespace.name,
+            "-k",
+            f"{path}/{project.namespace.name}",
+        ],
+        check=True,
     )
